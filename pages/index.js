a8bc0622da2dde72454dced681b915b2c64afc17
Tinu George <tinu.george@outlook.com>
2020-10-07 18:18:12 +0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div>
+            <div className="py-20">
+                <h1 className="text-5xl text-center text-accent-1">
+                    GamerSocial
+                </h1>
+            </div>
+        </div>
+    );
+}
